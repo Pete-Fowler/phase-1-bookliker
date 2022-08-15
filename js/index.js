f4363ee51ddc8a9a16d652b936d01ec0ccf07acc
Pete Fowler <fowler.pete@gmail.com>
2022-08-15 17:33:31 -0600

    Add titles, add thumbnail and description
    Add style to ul

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
-document.addEventListener("DOMContentLoaded", function() {});
+const displayDetails = (book) => {
+  const panel = document.querySelector('div#show-panel');
+  const box = document.createElement('div');
+  const img = document.createElement('img');
+  img.src = book.img_url;
+  const description = document.createElement('p');
+  description.textContent = book.description;
+  const likers = document.createElement('p');
+  likers.textContent = book.likers;
+  box.append(img, description, likers);
+  panel.append(box);
+  console.log(book);
+}
+
+const displayTitles = (data) => {
+  const list = document.querySelector('ul#list');
+  for(const obj in data) {
+    const book = data[obj];
+    const title = document.createElement('li');
+    title.textContent = book.title;
+    list.append(title);
+    title.addEventListener('click', () => displayDetails(book));
+  }
+}
+
+const getTitles = (e) => {
+  return fetch('http://localhost:3000/books')
+  .then(res => res.json())
+  .then(data => displayTitles(data));
+}
+
+document.addEventListener("DOMContentLoaded", getTitles);
+
+// When a user clicks the title of a book, display the book's thumbnail, 
+// description, and a list of users who have liked the book. This information 
+// should be displayed in the div#show-panel element.
